346e8b626a74d0e7e838f82823f320e511d17236
srinivasdevopsa <dbvasu13@gmail.com>
2024-01-26 23:14:16 +0530

    Adding Login Page

---
+++ pages/loginPage.ts
@@ -0,0 +1,41 @@
+import{test,expect,type Page,type Locator} from '@playwright/test'
+import * as data from '../testData/data.json'
+export class LoginPage{
+    private page:Page
+    readonly inputUserName:Locator
+    readonly inputPassWord:Locator
+    readonly btnSubmit:Locator
+    readonly btnLogout:Locator
+    readonly locattor:Locator
+    readonly error:Locator
+    constructor( page : Page){
+        this.page = page;
+        this.inputUserName = page.locator('//input[@id="username"]');
+        this.inputPassWord = page.locator('//input[@id="password"]');
+        this.btnSubmit = page.locator('//button[@id="submit"]');
+        this.btnLogout = page.locator('//a[contains(text(),"Log out")]');
+        this.locattor = page.locator('div[class=post-content] strong');
+        this.error=page.locator('//div[@id="error"][@class="show"]');
+    }
+    async login(page) {
+
+    await this.page.goto('./');
+    await this.inputUserName.fill(data.userName);
+    await this.inputPassWord.fill(data.passWord);
+    await this.btnSubmit.click();
+    await this.page.waitForTimeout(2000);
+    await expect(this.page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+    await expect(this.locattor).toHaveText('Congratulations student. You successfully logged in!');
+    await this.btnLogout.click();
+
+    }
+    async checkInvalidUserName(page){
+        await this.inputUserName.fill(data.invaliduserName);
+        await this.inputPassWord.fill(data.passWord);
+        await this.btnSubmit.click();
+        await expect(this.error).toBeVisible();
+        await expect(this.error).toHaveText('Your username is invalid!')
+    }
+
+    
+}
---
+++ tests/sampleTestcase1.spec.ts
@@ -0,0 +1,40 @@
+import {test,expect} from '@playwright/test'
+import { LoginPage } from '../pages/loginPage';
+
+test.only('open url',async({page})=>{
+     let loginPage= new LoginPage(page)
+
+await loginPage.login(page)
+
+    // await page.goto('https://practicetestautomation.com/practice-test-login/');
+    // await page.locator('//input[@id="username"]').fill('student');
+    // await page.locator('//input[@id="password"]').fill('Password123');
+    // await page.locator('//button[@id="submit"]').click();
+    // await page.waitForTimeout(2000);
+    // await expect(page).toHaveURL('https://practicetestautomation.com/logged-in-successfully/');
+    // const locattor = page.locator('div[class=post-content] strong');
+    // await expect(locattor).toHaveText('Congratulations student. You successfully logged in!');
+    // await page.locator('//a[contains(text(),"Log out")]').click();
+});
+
+test.only('verify with incorrect username ',async({page}) =>{
+    await page.goto('https://practicetestautomation.com/practice-test-login/');
+
+    let loginPage =new LoginPage(page)
+
+    await loginPage.checkInvalidUserName(page)
+   
+    // await page.goto('https://practicetestautomation.com/practice-test-login/');
+    // await page.locator('//input[@id="username"]').fill('srinivas');
+    // await page.locator('//input[@id="password"]').fill('Password123');
+    // await page.locator('//button[@id="submit"]').click();
+    // await page.waitForTimeout(5000);
+
+});
+test.only('verfiy with incorrect password',async({page})=>{
+    // await page.goto('https://practicetestautomation.com/practice-test-login/');
+    // await page.locator('//input[@id="username"]').fill('student');
+    // await page.locator('//input[@id="password"]').fill('Password@123');
+    // await page.locator('//button[@id="submit"]').click();
+    // await page.waitForTimeout(5000);
+})
